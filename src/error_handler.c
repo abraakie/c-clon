86607f75a9a321bd79408239676f88fcbd9f1ec9
Aki Abramowski <abraakie@web.de>
2025-07-05 14:34:57 +0200

    add error formatting tests

--- src/error_handler.c
+++ src/error_handler.c
@@ -7,19 +7,21 @@
 size_t format_error(const Error * error, char * buffer, size_t buffer_len) {
     if (!error || !buffer || !buffer_len) return 0;
 
-    if (error->type == ERR_BUFFER_OVERFLOW) return snprintf(buffer, buffer_len, "Buffer overflow");
-    if (error->type == ERR_MEMORY) return snprintf(buffer, buffer_len, "Memory allocation failed");
+    if (error->type == ERR_BUFFER_OVERFLOW) return snprintf(buffer, buffer_len, "Buffer overflow: %s", error->message);
+    if (error->type == ERR_MEMORY) return snprintf(buffer, buffer_len, "Memory allocation failed: %s", error->message);
 
     size_t written = snprintf(buffer, buffer_len, "%s\n", error->input);
     if (written >= buffer_len) return buffer_len;
 
-    const size_t token_start_pos = error->token_start - error->input;
-    for (size_t i = 0; i < token_start_pos && written < buffer_len; ++i) {
-        buffer[written++] = ' ';
+    if (error->token_length > 0) {
+        const size_t token_start_pos = error->token_start - error->input;
+        for (size_t i = 0; i < token_start_pos && written < buffer_len; ++i) {
+            buffer[written++] = ' ';
+        }
+        if (written >= buffer_len) return buffer_len;
+        written += snprintf(buffer + written, buffer_len - written, "%.*s\n", (int) error->token_length, error->token_start);
+        if (written >= buffer_len) return buffer_len;
     }
-    if (written >= buffer_len) return buffer_len;
-    written += snprintf(buffer + written, buffer_len - written, "%.*s\n", (int) error->token_length, error->token_start);
-    if (written >= buffer_len) return buffer_len;
 
     for (size_t i = 0; i < error->pos && written < buffer_len; ++i) {
         buffer[written++] = ' ';
---
+++ test/error_handler_test.c
@@ -0,0 +1,65 @@
+//
+// Created by Aki Abramowski on 05.07.25.
+//
+
+#include "unity.h"
+#include "error_handler.h"
+
+#include <stdlib.h>
+
+void setUp() {}
+void tearDown() {}
+
+void test_syntax_error_formatting() {
+    const char * input = "my_awesome_faulty!input";
+    Error error;
+    make_syntax_error(&error, input, &input[17], 1, 0, "My Error");
+    const char * expected =
+        "my_awesome_faulty!input\n"
+        "                 !\n"
+        "                 ^\n"
+        "My Error";
+    const char actual[512] = {0};
+    format_error(&error, actual, sizeof(actual));
+    TEST_ASSERT_EQUAL_STRING(expected, actual);
+}
+
+void test_syntax_error_formatting_eof_token() {
+    const char * input = "my_awesome_faulty_input=";
+    Error error;
+    make_syntax_error(&error, input, &input[24], 0, 0, "My Missing Value Error");
+    const char * expected =
+        "my_awesome_faulty_input=\n"
+        "                        ^\n"
+        "My Missing Value Error";
+    const char actual[512] = {0};
+    format_error(&error, actual, sizeof(actual));
+    TEST_ASSERT_EQUAL_STRING(expected, actual);
+}
+
+void test_memory_error_formatting() {
+    Error error;
+    make_memory_error(&error, "My Mem Err");
+    const char * expected = "Memory allocation failed: My Mem Err";
+    const char actual[512] = {0};
+    format_error(&error, actual, sizeof(actual));
+    TEST_ASSERT_EQUAL_STRING(expected, actual);
+}
+
+void test_buffer_overflow_error_formatting() {
+    Error error;
+    make_buffer_overflow_error(&error, "My Buffer Overflow Err");
+    const char * expected = "Buffer overflow: My Buffer Overflow Err";
+    const char actual[512] = {0};
+    format_error(&error, actual, sizeof(actual));
+    TEST_ASSERT_EQUAL_STRING(expected, actual);
+}
+
+int main() {
+    UNITY_BEGIN();
+    RUN_TEST(test_syntax_error_formatting);
+    RUN_TEST(test_syntax_error_formatting_eof_token);
+    RUN_TEST(test_memory_error_formatting);
+    RUN_TEST(test_buffer_overflow_error_formatting);
+    return UNITY_END();
+}
